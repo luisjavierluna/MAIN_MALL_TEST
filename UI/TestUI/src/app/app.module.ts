d450d13c5b607efc9978db1b0b72a0bec5e12972
luisjavierluna <javierlunaluis@gmail.com>
2022-10-16 18:47:27 -0500

    Refactorized menu items

--- UI/TestUI/src/app/app.module.ts
+++ UI/TestUI/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { MegaMenuItemsComponent } from './header-menu/mega-menu-items/mega-menu-items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderMenuComponent,
+    MegaMenuItemsComponent
   ],
   imports: [
     BrowserModule
---
+++ UI/TestUI/src/app/header-menu/mega-menu-items/mega-menu-items.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mega-menu-items',
+  templateUrl: './mega-menu-items.component.html',
+  styleUrls: ['./mega-menu-items.component.css']
+})
+export class MegaMenuItemsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
