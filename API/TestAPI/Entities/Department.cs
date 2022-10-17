4c7537aa5a648b0bf392bd1c96d58ffbad0eb7f4
luisjavierluna <javierlunaluis@gmail.com>
2022-10-17 14:02:10 -0500

    Added backend entities

---
+++ API/TestAPI/Entities/Department.cs
@@ -0,0 +1,9 @@
+﻿namespace TestAPI.Entities
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DepartmentName { get; set; }
+        public DepartmentArea[] DepartmentAreas { get; set; }
+    }
+}
---
+++ API/TestAPI/Entities/DepartmentArea.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestAPI.Entities
+{
+    public class DepartmentArea
+    {
+        [Key]
+        public int Id { get; set; }
+        public string AreaName { get; set; }
+        public string[] SubAreas{ get; set; }
+    }
+}
